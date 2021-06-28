aab90c01c535355be8a3cc38c4710ddbc83bf694
John Korsnes <johnkors@gmail.com>
2021-06-28 13:12:28 +0200

    x-schmol

--- minimize.sh
+++ minimize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for filename in /stageprofiles/*.jpeg; do
     for ((i=1; i<=21; i++)); do
-        npx jimp read ./stageprofiles/$i.jpeg  -a resize 480 -1 --output ./stageprofiles/schmol/$i.jpeg
+        npx jimp read ./stageprofiles/$i.jpeg  -a resize 240 -1 --output ./stageprofiles/x-schmol/$i.jpeg
     done
 done
